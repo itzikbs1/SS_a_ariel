d6b38d2070a11f38f1fbc532bf9b82323a8f886b
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2021-11-06 14:17:19 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 
-int checkArm(int x){
-    int numOfDigits = (int)log10(x);
-    if(x == 0){
-    return 0;
+int length_of_the_number(int x){
+    int count=0;
+    while(x>0){
+        count++;
+        x=x/10;
     }
- return((pow(x%10, numOfDigits)) + checkArm(x/10));
-
-
+    return count;
+}
+int power(int x, unsigned int y){
+    if (y == 0)
+        return 1;
+    if (y % 2 != 0)
+        return x*power(x, y / 2) * power(x, y / 2);
+    return power(x, y / 2) * power(x, y / 2);
 }
 
 int isArmstrong(int x){
-    if (x == checkArm(x)){
-        return 1;
-    }
-        return 0;
+     int digits = length_of_the_number(x)
+        int sum = 0;
+        int temp = x;
+        int r = 0;
+        while(temp > 0){
+            r = temp % 10;
+            temp = temp / 10;
+            sum += power(r,digits);   
+        }
+        if(sum == x){
+            return 1;
+        }else{
+            return 0;
+        }
 }
 
 int checkPal(int x){
     int numOfDigits = (int)log10(x);
-    if(x == 0){
+    if(x == 0)
     return 0;
-    }
+
     return((x%10 * pow(10, numOfDigits)) + checkPal(x/10));
 }
 
