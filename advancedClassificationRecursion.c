b9bb39f918da2641330b9c2002b9270da9b79856
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2021-11-06 10:54:05 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -3,32 +3,33 @@
 
 int checkArm(int x){
     int numOfDigits = (int)log10(x);
-    // int y = 3;
-    if(x == 0)
+    if(x == 0){
     return 0;
-
+    }
  return((pow(x%10, numOfDigits)) + checkArm(x/10));
 
 
 }
 
 int isArmstrong(int x){
-    if (x == checkArm(x))
+    if (x == checkArm(x)){
         return 1;
+    }
         return 0;
 }
 
 int checkPal(int x){
     int numOfDigits = (int)log10(x);
-    if(x == 0)
+    if(x == 0){
     return 0;
-
+    }
     return((x%10 * pow(10, numOfDigits)) + checkPal(x/10));
 }
 
 int isPalindrome(int x){
     
-    if(x == checkPal(x))
+    if(x == checkPal(x)){
     return 1;
+    }
     return 0;
 }
